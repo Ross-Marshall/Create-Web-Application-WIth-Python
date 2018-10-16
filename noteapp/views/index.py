582de6cbc40ea08c270a854fdec95f560c5ae753
Ross-Marshall <rossmarshallgithub@gmail.com>
2018-10-15 21:35:58 -0400

    created index

--- noteapp/views/index.py
+++ noteapp/views/index.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 bp = Blueprint(__name__, __name__, template_folder='templates')
 
-bp.route('/')
+@bp.route('/')
 def show():
-	return 'Hello World'
+	return render_template('index.html')
