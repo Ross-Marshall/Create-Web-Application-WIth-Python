e4961d9f14c7c3216ae0ca70be811ac110b4ae64
Ross-Marshall <rossmarshallgithub@gmail.com>
2018-10-20 04:34:17 -0400

    Add files via upload

--- noteapp/views/createnote.py
+++ noteapp/views/createnote.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 bp = Blueprint(__name__, __name__, template_folder='templates')
 
-@bp.route('/createnote')
+@bp.route('/createnote', methods=['POST','GET'])
 def show():
 	return render_template('createnote.html')
