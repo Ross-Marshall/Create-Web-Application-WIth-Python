056c3865c2fd4f91cde39b9016a7a2c7fa13cdca
Ross-Marshall <rossmarshallgithub@gmail.com>
2018-10-20 22:34:30 -0400

    random

--- noteapp/views/createnote.py
+++ noteapp/views/createnote.py
@@ -1,7 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import random
 
 bp = Blueprint(__name__, __name__, template_folder='templates')
 
+def random_string(length=16):
+    final_string = ''
+    chars = 'abcdefghijklmnopqrxtuvwxyzABCDEFGHIJKLMNOPQRXTUVWXYZ1234567890'
+
+    for i in range(0, length):
+        final_string += chars[ random.randint(0, len(chars)-1) ]
+    
+    return final_string
+
 @bp.route('/createnote', methods=['POST','GET'])
 def show():
-	return render_template('createnote.html')
+    if request.method == 'POST':
+        if request.form.get('createnote'):
+            text = request.form.get('notetext')
+
+            return random_string()
+
+    return render_template('createnote.html')
